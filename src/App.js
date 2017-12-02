ffb4c7887b72bdfaf8a651d82f0fca08abe1b723
Iggy <igoririanto@rocketmail.com>
2017-12-02 11:21:18 -0800

    able to get info from medium

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+var data = {rss_url: 'https://medium.com/feed/@stupendous_igg'}
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isLoading: true,
+      axiosResponse: {}
+    }
+  };
+
+  componentDidMount(){
+    var that = this;
+    axios.get('https://api.rss2json.com/v1/api.json', {
+        params: data
+      })
+      .then(function (response) {
+        console.log(response);
+        setTimeout(function() {
+          that.setState({
+            axiosResponse: response.data,
+            isLoading: false
+          })
+        }, 1500); 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   render() {
+    if(this.state.isLoading){
+      return <div>Waiting...</div>
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">{this.state.axiosResponse.feed.url}</h1>
       </div>
     );
   }
